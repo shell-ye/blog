e6e9b7886de0226d3bbf19fdfcb6e48a9af50177
shell-ye <15070035244@163.com>
2020-10-12 13:33:42 +0800

    移动端登录

--- server/routes/users.js
+++ server/routes/users.js
@@ -30,8 +30,18 @@ router.get('/token', (req, res) => {
 })
 
 // 注册发送邮箱验证码
-router.post('/register/email', (req, res) => {
+router.post('/register/email', async (req, res) => {
   if ( req.body.email ) {
+    // 查询邮箱
+    let result = await db.select('*').from('user').where('email', req.body.email).queryRow().catch(err => {
+      console.log( err )
+      res.send({code: 0, msg: '系统繁忙'})
+      return
+    })
+    if ( result != undefined ) {
+      res.send({code: 0, msg: '该邮箱已被注册'})
+      return
+    } 
     let random_code = parseInt( Math.random() * 1000000 ).toString()
     sandEmail( req.body.email,random_code ).then( data => {
       req.session.email = req.body.email
@@ -51,15 +61,8 @@ router.post('/register', async (req, res) => {
   if ( !req.body.password ) { return res.send({code: '000011', msg: '缺少参数 password'}) }
   if ( !req.body.emailms ) { return res.send({code: '000012', msg: '缺少参数 emailms'}) }
   if ( req.body.email && req.body.password && req.body.emailms ) {
-    // 查询邮箱
-    let result = await db.select('*').from('user').where('email', req.body.email).queryRow().catch(err => {
-      req.session.email = req.body.email
-      req.session.code = random_code
-      res.send({ code: 200, data })
-    })
-    if ( result != undefined ) {
-      res.send({code: 0, msg: '该邮箱已被注册'})
-      return
+    if ( req.body.emailms != req.session.code ) {
+      res.send({code: 0, msg: '验证码错误'})
     } else {
       // 插入数据
       await db.insert('user').column('email', req.body.email).column('password', req.body.password).execute().catch(err => {
